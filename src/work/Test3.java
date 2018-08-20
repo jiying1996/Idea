7713a269458e16ffba09b3224b71d679e2b9f440
wqk <1663921264@qq.com>
2018-08-20 19:19:28 +0800

    1

--- src/work/Test3.java
+++ src/work/Test3.java
@@ -10,7 +10,7 @@ public class Test3 {
         /*
         3.将当前的工程目录中后缀名为.txt的文件找出,并将内容添加到名字叫zong.txt的文件中,并将该文件重新放到工程目录/day21/zuoye/third/下面.
          */
-        SearchAllFiles("E:\\idea\\IODemo", "E:\\Test");
+        SearchAllFiles("E:\\Idea\\IODemo", "E:\\Test");
     }
 
     public static void SearchAllFiles(String pathName,String desPath) throws IOException {
@@ -22,7 +22,7 @@ public class Test3 {
         FileOutputStream fileOutputStream = new FileOutputStream(desPath+File.separator+"zong.txt");
         if (file.exists()) {
             if (file.isFile()) {
-                if(file.getName().endsWith(".txt")){
+                if(file.getName().trim().endsWith(".txt")){
                     fileOutputStream.write(file.getName().getBytes());
                 }
             } else {
@@ -31,14 +31,13 @@ public class Test3 {
                     if (file1.isDirectory()) {
                         SearchAllFiles(pathName + File.separator + file1.getName(),desPath);
                     } else {
-                        if(file.getName().endsWith(".txt")){
-                            fileOutputStream.write(file.getName().getBytes());
+                        if(file.getName().trim().endsWith(".txt")){
+                            System.out.println(file.getName().getBytes());
+//                            fileOutputStream.write(file.getName().getBytes());
                         }
                     }
                 }
             }
-        } else {
-            System.out.println("目录不存在");
         }
     }
 }
