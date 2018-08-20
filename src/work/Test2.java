36e3d470f708c1b84d2f1f3ae0cd9feb119db9fe
wqk <1663921264@qq.com>
2018-08-20 19:16:17 +0800

    123

--- src/work/Test2.java
+++ src/work/Test2.java
@@ -5,12 +5,12 @@ import java.io.File;
 public class Test2 {
     public static void main(String[] args) {
         /*
-        作业：遍历当前目录下所有的文件 -- 递归
+        2.删除指定目录下的文件及文件夹，包括子目录中的内容(当前的工程目录)
          */
-        listAllFiles("E:\\test");
+        deleteAllFiles("E:\\test");
     }
 
-    public static void listAllFiles(String pathName) {
+    public static void deleteAllFiles(String pathName) {
         File file = new File(pathName);
         if (file.exists()) {
             if (file.isFile()) {
@@ -19,7 +19,7 @@ public class Test2 {
                 File[] files = file.listFiles();
                 for (File file1 : files) {
                     if (file1.isDirectory()) {
-                        listAllFiles(pathName + File.separator + file1.getName());
+                        deleteAllFiles(pathName + File.separator + file1.getName());
                     } else {
                         file1.delete();
                     }
--- src/work/Test3.java
+++ src/work/Test3.java
@@ -1,7 +1,44 @@
 package work;
 
-public class Test3 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+public class Test3 {
+    public static void main(String[] args) throws IOException {
+        /*
+        3.将当前的工程目录中后缀名为.txt的文件找出,并将内容添加到名字叫zong.txt的文件中,并将该文件重新放到工程目录/day21/zuoye/third/下面.
+         */
+        SearchAllFiles("E:\\idea\\IODemo", "E:\\Test");
+    }
+
+    public static void SearchAllFiles(String pathName,String desPath) throws IOException {
+        File file = new File(pathName);
+        File file2 = new File(desPath);
+        if(!file2.exists()){
+            file2.mkdirs();
+        }
+        FileOutputStream fileOutputStream = new FileOutputStream(desPath+File.separator+"zong.txt");
+        if (file.exists()) {
+            if (file.isFile()) {
+                if(file.getName().endsWith(".txt")){
+                    fileOutputStream.write(file.getName().getBytes());
+                }
+            } else {
+                File[] files = file.listFiles();
+                for (File file1 : files) {
+                    if (file1.isDirectory()) {
+                        SearchAllFiles(pathName + File.separator + file1.getName(),desPath);
+                    } else {
+                        if(file.getName().endsWith(".txt")){
+                            fileOutputStream.write(file.getName().getBytes());
+                        }
+                    }
+                }
+            }
+        } else {
+            System.out.println("目录不存在");
+        }
     }
 }
